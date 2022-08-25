dedaf0261d5d1ba70a9ebb454187f84ac783a22b
Lucas Fiorelli <fiorellilucas@gmail.com>
2022-08-25 19:03:16 -0300

    Mostrar todos os filmes na home

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactComponentElement, useEffect, useState } from "react"
+import "./App.css"
+
+type Filme = {
+  ano_lancamento: Number
+  diretor: String
+  distribuidora: String
+  roteirista: String
+  titulo: String
+  elenco_principal: String[]
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ListaFilmes />
     </div>
-  );
+  )
 }
 
-export default App;
+function ListaFilmes() {
+  const [colecaoFilmes, setColecaoFilmes] = useState<Array<Filme>>([])
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((filmes) => {
+        setColecaoFilmes(filmes)
+      })
+  }, [])
+
+  const todosFilmes = colecaoFilmes.map((filme) => {
+    return (
+      <FilmeComponent
+        titulo={filme.titulo}
+        ano_lancamento={filme.ano_lancamento}
+        diretor={filme.diretor}
+        distribuidora={filme.distribuidora}
+        elenco_principal={filme.elenco_principal}
+        roteirista={filme.roteirista}
+      />
+    )
+  })
+
+  return <ul>{todosFilmes}</ul>
+}
+
+function FilmeComponent(props: Filme) {
+  return (
+    <li>
+      <h2>{props.titulo}</h2>
+      <h3>{props.ano_lancamento.toString()}</h3>
+      <p><strong>Diretor: {props.diretor}</strong></p>
+      <p>Roteirista: {props.roteirista}</p>
+      <p>Elenco principal:</p>
+      <ul>
+        <li>{props.elenco_principal[0]}</li>
+        <li>{props.elenco_principal[1]}</li>
+        <li>{props.elenco_principal[2]}</li>
+      </ul>
+      <p>Distribuidora: {props.distribuidora}</p>
+    </li>
+  )
+}
+
+export default App
--- server.ts
+++ server.ts
@@ -1,8 +1,18 @@
 import express, { Express, Request, Response } from "express"
 import { FirebaseApp, initializeApp } from "firebase/app"
-import { Firestore, getFirestore, setDoc, doc } from "firebase/firestore"
-
+import {
+  Firestore,
+  getFirestore,
+  getDocs,
+  DocumentSnapshot,
+  collection,
+  CollectionReference,
+  QuerySnapshot,
+  setDoc,
+  doc
+} from "firebase/firestore"
 import "dotenv/config"
+import path from "path"
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
@@ -11,7 +21,7 @@ const firebaseConfig = {
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  measurementId: process.env.MEASUREMENT_ID,
 }
 
 const fb_app: FirebaseApp = initializeApp(firebaseConfig)
@@ -20,20 +30,32 @@ const db: Firestore = getFirestore(fb_app)
 const app: Express = express()
 const PORT: Number = 5000
 
-app.get("/", async (req: Request, res: Response) => {
-  await setDoc(doc(db, "Filmes", "De Volta Para o Futuro"), {
-    titulo: "De Volta Para o Futuro",
-    ano_lancamento: 1985,
-    diretor: "Steven Spielberg",
-    roteirista: "Robert Zemeckis",
-    distribuidora: "Universal Pictures",
-    elenco_principal: [
-      "Michael J. Fox",
-      "Christopher Lloyd",
-      "Lea Thompson"
-    ] 
+const colRef: CollectionReference = collection(db, "Filmes")
+
+app.get("/api", async (req: Request, res: Response) => {
+  
+  // await setDoc(doc(db, "Filmes", "Interstellar"), {
+  //   titulo: "Interstellar",
+  //   ano_lancamento: 2014,
+  //   diretor: "Christopher Nolan",
+  //   roteirista: "Jonathan Nolan",
+  //   distribuidora: "Warner Bros. Pictures",
+  //   elenco_principal: [
+  //     "Matthew McConaughey",
+  //     "Anne Hathaway",
+  //     "Jessica Chastain"
+  //   ]
+  // })
+
+  const querySnap: QuerySnapshot = await getDocs(colRef)
+
+  let todosFilmes: any = []
+
+  querySnap.forEach((doc: DocumentSnapshot) => {
+    todosFilmes.push(doc.data())
   })
-  res.end()
+  
+  res.json(todosFilmes)
 })
 
 app.listen(PORT, () => {
